70a65491edeb35fd7bcabac4b28abb094fa22f0e
Chaedie <chaedong.im.dev@gmail.com>
2022-12-20 22:56:00 +0900

    Style: 포트폴리오 카드 애니메이션 추가

--- src/components/portfolio/PortfolioList.tsx
+++ src/components/portfolio/PortfolioList.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PortfolioItem from './PortfolioItem';
 import classes from './portfolio-list.module.css';
-
-interface Props {}
+import { Collapse } from '@mui/material';
 
 export default function PortfolioList() {
+  const [grow, setGrow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setGrow(true);
+    }, 50);
+  }, []);
+
   return (
-    <ul className={classes.list}>
-      {PORTFOLIO_LIST.map(portfolio => {
-        const { id, title, description, date, image, linkUrl } = portfolio;
-        return (
-          <PortfolioItem
-            key={id}
-            id={'1'}
-            title={title}
-            description={description}
-            date={date}
-            image={image}
-            linkUrl={linkUrl}
-          />
-        );
-      })}
-    </ul>
+    <Collapse in={grow}>
+      <ul className={classes.list}>
+        {PORTFOLIO_LIST.map(portfolio => {
+          const { id, title, description, date, image, linkUrl } = portfolio;
+          return (
+            <PortfolioItem
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              date={date}
+              image={image}
+              linkUrl={linkUrl}
+            />
+          );
+        })}
+      </ul>
+    </Collapse>
   );
 }
 
@@ -34,4 +43,12 @@ const PORTFOLIO_LIST = [
     image: 'https://user-images.githubusercontent.com/88099590/203834370-68fd5cb3-c250-490a-ade9-cf66d8414836.gif',
     linkUrl: 'https://github.com/Chaedie/simple-todo-list',
   },
-];
+  {
+    id: '2',
+    title: 'Next Portfolio',
+    description: '기술 스택 학습을 위한 장난감 프로젝트',
+    date: '2022-10',
+    image: 'https://user-images.githubusercontent.com/88099590/203834370-68fd5cb3-c250-490a-ade9-cf66d8414836.gif',
+    linkUrl: 'https://github.com/Chaedie/simple-todo-list',
+  },
+].reverse();
