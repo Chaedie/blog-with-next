ad0e9582f7abc67c832d0b7bcd8752da3eddc578
ChaedongIm <chaedong.im.dev@gmail.com>
2022-12-21 19:31:28 +0900

    Feature/portfolio (#1)
    * Style: 포트폴리오 카드 애니메이션 추가
    
    * Add: 포트폴리오 추가, Type 추가

--- src/components/portfolio/PortfolioItem.tsx
+++ src/components/portfolio/PortfolioItem.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import Button from '../ui/button';
+import StyledButton from '../ui/button';
 import classes from './portfolio-item.module.css';
 import DateIcon from '../icons/date-icon';
 import InformationIcon from '../icons/information-circle';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import { Portfolio } from '../../types/portfolio';
 import Image from 'next/image';
+import { Button } from '@mui/material';
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
 
 interface Props {}
 
-export default function PortfolioItem({ title, image, date, id, linkUrl, description }: Portfolio) {
+export default function PortfolioItem({
+  title,
+  image,
+  imageWidth,
+  imageHeight,
+  date,
+  type,
+  id,
+  linkUrl,
+  description,
+}: Portfolio) {
   const humanReadableDate = new Date(date).toLocaleDateString('ko-KR', {
     month: 'long',
     year: 'numeric',
@@ -17,10 +29,14 @@ export default function PortfolioItem({ title, image, date, id, linkUrl, descrip
 
   return (
     <li className={classes.item}>
-      <Image src={image} alt={title} width={100} height={100} />
+      <Image src={image} alt={title} width={imageWidth} height={imageHeight} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
+        </div>
+        <div className={classes.type + ' ' + classes[typeStyle(type)]}>
+          <StickyNote2Icon />
+          <span>{type}</span>
         </div>
         <div className={classes.description}>
           <InformationIcon />
@@ -31,14 +47,23 @@ export default function PortfolioItem({ title, image, date, id, linkUrl, descrip
           <time>{humanReadableDate}</time>
         </div>
         <div className={classes.actions}>
-          <Button link={linkUrl}>
+          <StyledButton link={linkUrl}>
             <span>Go to Project</span>
             <span className={classes.icon}>
               <ArrowRightIcon />
             </span>
-          </Button>
+          </StyledButton>
         </div>
       </div>
     </li>
   );
 }
+
+const typeStyle = (type: string) => {
+  if (type === '개인 프로젝트') {
+    return 'secondary';
+  } else if (type === 'Front-End Challenge') {
+    return 'primary';
+  }
+  return 'primary';
+};
--- src/components/portfolio/PortfolioList.tsx
+++ src/components/portfolio/PortfolioList.tsx
@@ -3,46 +3,62 @@ import PortfolioItem from './PortfolioItem';
 import classes from './portfolio-list.module.css';
 import { Collapse } from '@mui/material';
 
-interface Props {}
-
 export default function PortfolioList() {
   const [grow, setGrow] = useState(false);
-  const timeout = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
-    timeout.current = setTimeout(() => {
+    setTimeout(() => {
       setGrow(true);
-    }, 0);
-    return () => clearTimeout(timeout.current);
+    }, 50);
   }, []);
+
   return (
-    <ul className={classes.list}>
-      {PORTFOLIO_LIST.map(portfolio => {
-        const { id, title, description, date, image, linkUrl } = portfolio;
-        return (
-          <Collapse in={grow} key={id}>
+    <Collapse in={grow}>
+      <ul className={classes.list}>
+        {PORTFOLIO_LIST.map(portfolio => {
+          const { id, type, title, description, date, image, imageWidth, imageHeight, linkUrl } = portfolio;
+          return (
             <PortfolioItem
-              id={'1'}
+              key={id}
+              id={id}
+              type={type}
               title={title}
               description={description}
               date={date}
               image={image}
+              imageWidth={imageWidth}
+              imageHeight={imageHeight}
               linkUrl={linkUrl}
             />
-          </Collapse>
-        );
-      })}
-    </ul>
+          );
+        })}
+      </ul>
+    </Collapse>
   );
 }
 
 const PORTFOLIO_LIST = [
   {
     id: '1',
+    type: '개인 프로젝트',
     title: 'Simple Todo List',
     description: '기술 스택 학습을 위한 장난감 프로젝트',
     date: '2022-10',
     image: 'https://user-images.githubusercontent.com/88099590/203834370-68fd5cb3-c250-490a-ade9-cf66d8414836.gif',
+    imageWidth: 100,
+    imageHeight: 100,
     linkUrl: 'https://github.com/Chaedie/simple-todo-list',
   },
-];
+  {
+    id: '2',
+    type: 'Front-End Challenge',
+    title: 'Card Component',
+    description: 'HTML/CSS 챌린지 - Newbie Level',
+    date: '2022-12',
+    image: 'https://user-images.githubusercontent.com/88099590/208877180-57c19fa3-5193-4821-a6cf-b236869a8b36.png',
+    imageWidth: 600,
+    imageHeight: 600,
+    linkUrl:
+      'https://front-end-mentor-challenge-inky.vercel.app/1-newbie/1-product-preview-card-component-main/index.html',
+  },
+].reverse();
